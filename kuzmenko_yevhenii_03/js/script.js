b137455baa807da849d66a3ecb6ceb1dc44e83c4
KuzmenkoEugene <evgenkuzmenko94@gmail.com>
2023-03-07 00:02:29 +0200

    changed my homework

--- kuzmenko_yevhenii_03/js/script.js
+++ kuzmenko_yevhenii_03/js/script.js
@@ -4,40 +4,38 @@ if (choice <= 4 && choice > 0)  {
     const firstNumber = +prompt(`Введите число`);
     const secondNumber = +prompt(`Введите второе число`);
     if (1 === choice) {
-        const resultNumberSum = firstNumber + secondNumber
-        alert(`${firstNumber} + ${secondNumber} = ${resultNumberSum}`)
+        const resultNumberSum = firstNumber + secondNumber;
+        alert(`${firstNumber} + ${secondNumber} = ${resultNumberSum}`);
     } else if (2 === choice) {
-        const resultNumberDiff = firstNumber - secondNumber
-        alert(`${firstNumber} - ${secondNumber} = ${resultNumberDiff}`)
+        const resultNumberDiff = firstNumber - secondNumber;
+        alert(`${firstNumber} - ${secondNumber} = ${resultNumberDiff}`);
     } else if (3 === choice) {
-        const resultNumberMult = firstNumber * secondNumber
-        alert(`${firstNumber} * ${secondNumber} = ${resultNumberMult}`)
+        const resultNumberMult = firstNumber * secondNumber;
+        alert(`${firstNumber} * ${secondNumber} = ${resultNumberMult}`);
     } else if (4 === choice) {
-        const resultNumberDiv = firstNumber / secondNumber
-        alert(`${firstNumber} / ${secondNumber} = ${resultNumberDiv}`)
-    } else {
-    }
-} else if (choice == 5) {
+        const resultNumberDiv = firstNumber / secondNumber;
+        alert(`${firstNumber} / ${secondNumber} = ${resultNumberDiv}`);
+    } 
+} else if (choice === 5) {
     const firstNumber = +prompt(`Основание степени`);
     const secondNumber = +prompt(`Показатель степени`);
     if (5 === choice) {
         const resultPow = Math.pow(firstNumber, secondNumber);
-        alert(`Результат: ${resultPow}`)
-    } else {
-    }
-} else if (choice == 6) {
+        alert(`Результат: ${resultPow}`);
+    } 
+} else if (choice === 6) {
     const resultAdd = +prompt(`Выберите номер нужной вам операции: \n1. Cos \n2. Sin`);
     const myNumber = +prompt(`Введите число`);
-    if (resultAdd == 1) {
+    if (resultAdd === 1) {
         const resultCos = Math.cos(myNumber);
-        alert(`Результат: ${resultCos}`)
-    } else if (resultAdd == 2) {
+        alert(`Результат: ${resultCos}`);
+    } else if (resultAdd === 2) {
         const resultSin = Math.sin(myNumber);
-        alert(`Результат: ${resultSin}`)
+        alert(`Результат: ${resultSin}`);
     } else {
-        alert(`Ошибка! \nВы выбрали неправильный номер операции \nВы ввели "${resultAdd}".`)
+        alert(`Ошибка! \nВы выбрали неправильный номер операции \nВы ввели "${resultAdd}".`);
     }
 } else {
-    alert(`Ошибка! \nВы выбрали неправильный номер операции \nВы ввели "${choice}".`)
+    alert(`Ошибка! \nВы выбрали неправильный номер операции \nВы ввели "${choice}".`);
 }
 
--- kuzmenko_yevhenii_04/js/script.js
+++ kuzmenko_yevhenii_04/js/script.js
@@ -4,38 +4,35 @@ switch(true) {
     case choice === 1:
         const firstNumberSum = +prompt(`Введите число`);
         const secondNumberSum = +prompt(`Введите второе число`);
-        const resultNumberSum = firstNumberSum + secondNumberSum
+        const resultNumberSum = firstNumberSum + secondNumberSum;
         alert(`${firstNumberSum} + ${secondNumberSum} = ${resultNumberSum}`); break;
     case choice === 2:
         const firstNumberDiff = +prompt(`Введите число`);
         const secondNumberDiff = +prompt(`Введите второе число`);
-        const resultNumberDiff = firstNumberDiff - secondNumberDiff
+        const resultNumberDiff = firstNumberDiff - secondNumberDiff;
         alert(`${firstNumberDiff} - ${secondNumberDiff} = ${resultNumberDiff}`); break;
     case choice === 3:
         const firstNumberMult = +prompt(`Введите число`);
         const secondNumberMult = +prompt(`Введите второе число`);
-        const resultNumberMult = firstNumberMult * secondNumberMult
+        const resultNumberMult = firstNumberMult * secondNumberMult;
         alert(`${firstNumberMult} * ${secondNumberMult} = ${resultNumberMult}`); break;
     case choice === 4:
         const firstNumberDiv = +prompt(`Введите число`);
         const secondNumberDiv = +prompt(`Введите второе число`);
         const resultNumberDiv = firstNumberDiv / secondNumberDiv
         alert(`${firstNumberDiv} / ${secondNumberDiv} = ${resultNumberDiv}`); break;
-    case choice == 5: 
+    case choice === 5: 
         const firstNumberPow = +prompt(`Основание степени`);
         const secondNumberPow = +prompt(`Показатель степени`);
-        case 5 === choice: 
-            const resultPow = Math.pow(firstNumberPow, secondNumberPow);
-            alert(`Результат: ${resultPow}`); break;
+        const resultPow = Math.pow(firstNumberPow, secondNumberPow);
+        alert(`Результат: ${resultPow}`); break;
     case 6 === choice:
         const myNumberCos = +prompt(`Введите число`);
-        case choice == 6:
-            const resultCos = Math.cos(myNumberCos);
-            alert(`Результат: ${resultCos}`); break;
+        const resultCos = Math.cos(myNumberCos);
+        alert(`Результат: ${resultCos}`); break;
     case 7 === choice:    
         const myNumberSin = +prompt(`Введите число`);
-        case choice == 7:
-            const resultSin = Math.sin(myNumberSin);
-            alert(`Результат: ${resultSin}`); break;
+        const resultSin = Math.sin(myNumberSin);
+        alert(`Результат: ${resultSin}`); break;
     default: alert(`Ошибка! \nВы выбрали неправильный номер операции \nВы ввели "${choice}".`); break;
 }
