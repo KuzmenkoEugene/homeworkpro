77f89149ee08776ca790b5eadb66e880a519b2f9
KuzmenkoEugene <evgenkuzmenko94@gmail.com>
2023-03-11 01:15:33 +0200

    changed my homework

--- kuzmenko_yevhenii_05/js/script.js
+++ kuzmenko_yevhenii_05/js/script.js
@@ -1,11 +1,12 @@
 let firstNumber; 
 let secondNumber;
 let result;
+const myArray = [];
 
 do {
     do {
-        choice = +prompt(`Выберите номер нужной вам операции: \n1. Сложение  \n2. Вычетание \n3. Умножение \n4. Деление \n5. Возвести в степень \n6. Cos \n7. Sin`);
-    } while(choice > 7 || isNaN(choice) || choice === 0)
+        choice = +prompt(`Выберите номер нужной вам операции: \n1. Сложение  \n2. Вычетание \n3. Умножение \n4. Деление \n5. Возвести в степень \n6. Cos \n7. Sin \n8. History`);
+    } while(choice > 8 || choice === 0 || ! +choice)
     
     do {
         switch(choice) {
@@ -13,37 +14,53 @@ do {
             secondNumber = +prompt(`Введите второе число`); 
             result = firstNumber + secondNumber; 
             alert(`Operation sum finished with result ${result}`);
+            operation = `Operation sum finished with result ${result}`
+            myArray[myArray.length] = operation; 
             break;
             case 2: firstNumber = +prompt(`Введите число`);
             secondNumber = +prompt(`Введите второе число`); 
             result = firstNumber - secondNumber; 
             alert(`Operation subtract finished with result ${result}`);
+            operation = `Operation subtract finished with result ${result}`
+            myArray[myArray.length] = operation; 
             break;
             case 3: firstNumber = +prompt(`Введите число`);
             secondNumber = +prompt(`Введите второе число`);
             result = firstNumber * secondNumber; 
             alert(`Operation multiply finished with result ${result}`);
+            operation = `Operation multyply finished with result ${result}`
+            myArray[myArray.length] = operation; 
             break;
             case 4: firstNumber = +prompt(`Введите число`);
             secondNumber = +prompt(`Введите второе число`);
             result = firstNumber / secondNumber; 
             alert(`Operation division finished with result ${result}`);
+            operation = `Operation division finished with result ${result}`
+            myArray[myArray.length] = operation; 
             break;
             case 5: firstNumber = +prompt(`Основание степени`);
             secondNumber = +prompt(`Показатель степени`);
             result = Math.pow(firstNumber, secondNumber); 
             alert(`Operation pow finished with result ${result}`);
+            operation = `Operation pow finished with result ${result}`
+            myArray[myArray.length] = operation; 
             break;
             case 6: firstNumber = +prompt(`Введите число`);
             result = Math.cos(firstNumber); 
             alert(`Operation cos finished with result ${result}`);
+            operation = `Operation cos finished with result ${result}`
+            myArray[myArray.length] = operation; 
             break;
             case 7: firstNumber = +prompt(`Введите число`);
             result = Math.sin(firstNumber); 
             alert(`Operation sin finished with result ${result}`);
+            operation = `Operation sin finished with result ${result}`
+            myArray[myArray.length] = operation; 
             break;
-        }
+            case 8: alert(myArray); break;
+        } 
     } while(isNaN(firstNumber, secondNumber))
-    
+
     doItAgain = confirm(`Вы хотите совершить еще одну операцию?`)
+    
 } while(doItAgain === true)
