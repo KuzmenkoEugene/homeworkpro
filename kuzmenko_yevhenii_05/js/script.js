48ad6a8ee8a78d973449697bf8ab9b01b9e23c38
KuzmenkoEugene <evgenkuzmenko94@gmail.com>
2023-03-11 23:24:08 +0200

    changed my HW

--- kuzmenko_yevhenii_05/js/script.js
+++ kuzmenko_yevhenii_05/js/script.js
@@ -14,69 +14,76 @@ do {
             case 1: firstNumber = +prompt(`Введите число`);
             secondNumber = +prompt(`Введите второе число`); 
             result = firstNumber + secondNumber; 
-            operation = `Operation sum finished with result ${result}`;
-            myArray[myArray.length] = operation; 
+            
             break;
             case 2: firstNumber = +prompt(`Введите число`);
             secondNumber = +prompt(`Введите второе число`); 
             result = firstNumber - secondNumber; 
-            operation = `Operation subtract finished with result ${result}`;
-            myArray[myArray.length] = operation; 
+            
             break;
             case 3: firstNumber = +prompt(`Введите число`);
             secondNumber = +prompt(`Введите второе число`);
             result = firstNumber * secondNumber; 
-            operation = `Operation multyply finished with result ${result}`;
-            myArray[myArray.length] = operation; 
+            
             break;
             case 4: firstNumber = +prompt(`Введите число`);
             secondNumber = +prompt(`Введите второе число`);
             result = firstNumber / secondNumber; 
-            operation = `Operation division finished with result ${result}`;
-            myArray[myArray.length] = operation; 
+            
             break;
             case 5: firstNumber = +prompt(`Основание степени`);
             secondNumber = +prompt(`Показатель степени`);
             result = Math.pow(firstNumber, secondNumber); 
-            operation = `Operation pow finished with result ${result}`;
-            myArray[myArray.length] = operation; 
+            
             break;
             case 6: firstNumber = +prompt(`Введите число`);
             result = Math.cos(firstNumber); 
-            operation = `Operation cos finished with result ${result}`;
-            myArray[myArray.length] = operation; 
+            
             break;
             case 7: firstNumber = +prompt(`Введите число`);
             result = Math.sin(firstNumber); 
-            operation = `Operation sin finished with result ${result}`;
-            myArray[myArray.length] = operation; 
+            
             break;
+            case 8: myArrayCase = alert(myArray); break;
         } 
     } while(isNaN(firstNumber, secondNumber));
 
     switch(choice) {
         case 1: 
         alert(`Operation sum finished with result ${result}`);
+        operation = `Operation sum finished with result ${result}`;
+        myArray[myArray.length] = operation; 
         break;
         case 2: 
         alert(`Operation subtract finished with result ${result}`);
+        operation = `Operation subtract finished with result ${result}`;
+        myArray[myArray.length] = operation; 
         break;
         case 3: 
         alert(`Operation multiply finished with result ${result}`);
+        operation = `Operation multyply finished with result ${result}`;
+        myArray[myArray.length] = operation; 
         break;
         case 4: 
         alert(`Operation division finished with result ${result}`);
+        operation = `Operation division finished with result ${result}`;
+        myArray[myArray.length] = operation; 
         break;
         case 5: 
         alert(`Operation pow finished with result ${result}`);
+        operation = `Operation pow finished with result ${result}`;
+        myArray[myArray.length] = operation; 
         break;
         case 6: 
         alert(`Operation cos finished with result ${result}`);
+        operation = `Operation cos finished with result ${result}`;
+        myArray[myArray.length] = operation; 
         break;
         case 7: 
         alert(`Operation sin finished with result ${result}`);
+        operation = `Operation sin finished with result ${result}`;
+        myArray[myArray.length] = operation; 
         break;
-        case 8: myArrayCase = alert(myArray); break;
     };
 
     doItAgain = confirm(`Вы хотите совершить еще одну операцию?`);
