8ac8c22eb3043be774610062e4184f7ff9146f55
KuzmenkoEugene <evgenkuzmenko94@gmail.com>
2023-03-15 00:50:48 +0200

    changed my HW

--- kuzmenko_yevhenii_05/js/script.js
+++ kuzmenko_yevhenii_05/js/script.js
@@ -1,74 +1,91 @@
-let firstNumber; 
-let secondNumber;
 let result;
-const myArray = [];
+let secondNumber;
+let firstNumber;
+let operation;
+let myArray = [];
 
 do {
     do {
         choice = prompt(`Выберите номер нужной вам операции: \n1. Сложение  \n2. Вычетание \n3. Умножение \n4. Деление \n5. Возвести в степень \n6. Cos \n7. Sin \n8. History`);
     } while(isNaN(choice) || choice > 8 || choice == 0);
-    
-    do {
-        if (choice == 1) {
-            firstNumber = +prompt(`Введите число`);
-            secondNumber = +prompt(`Введите второе число`); 
-            result = firstNumber + secondNumber; 
-        } else if (choice == 2) {
-            firstNumber = +prompt(`Введите число`);
-            secondNumber = +prompt(`Введите второе число`); 
-            result = firstNumber - secondNumber; 
-        } else if (choice == 3) {
-            firstNumber = +prompt(`Введите число`);
-            secondNumber = +prompt(`Введите второе число`);
-            result = firstNumber * secondNumber; 
-        } else if (choice == 4) {
-            firstNumber = +prompt(`Введите число`);
-            secondNumber = +prompt(`Введите второе число`);
-            result = firstNumber / secondNumber; 
-        } else if (choice == 5) {
-            firstNumber = +prompt(`Основание степени`);
-            secondNumber = +prompt(`Показатель степени`);
-            result = Math.pow(firstNumber, secondNumber); 
-        } else if (choice == 6) {
-            firstNumber = +prompt(`Введите число`);
-            result = Math.cos(firstNumber); 
-        } else if (choice == 7) {
-            firstNumber = +prompt(`Введите число`);
-            result = Math.sin(firstNumber); 
-        } else if (choice == 8) {
-            alert(myArray);
-        } 
-    } while(!result);
 
     if (choice == 1) {
-        alert(`Operation sum finished with result ${result}`);
-        operation = `Operation sum finished with result ${result}`;
-        myArray[myArray.length] = operation; 
+        do {
+            firstNumber = +prompt(`Введите число`);
+        } while(!firstNumber);
+        do {
+            secondNumber = +prompt(`Введите второе число`);
+        } while(!secondNumber);
+        result = firstNumber + secondNumber;
+        operation = `Operation сложение, ${firstNumber} + ${secondNumber} = ${result}`;
+        myArray[myArray.length] = operation;
+        alert(`Operation сложение, ${firstNumber} + ${secondNumber} = ${result}`);
     } else if (choice == 2) {
-        alert(`Operation subtract finished with result ${result}`);
-        operation = `Operation subtract finished with result ${result}`;
-        myArray[myArray.length] = operation; 
+        do {
+            firstNumber = +prompt(`Введите число`);
+        } while(!firstNumber);
+        do {
+            secondNumber = +prompt(`Введите второе число`);
+        } while(!secondNumber);
+        result = firstNumber - secondNumber;
+        operation = `Operation вычетание, ${firstNumber} - ${secondNumber} = ${result}`;
+        myArray[myArray.length] = operation;
+        alert(`Operation вычетание, ${firstNumber} - ${secondNumber} = ${result}`);
     } else if (choice == 3) {
-        alert(`Operation multiply finished with result ${result}`);
-        operation = `Operation multyply finished with result ${result}`;
-        myArray[myArray.length] = operation; 
+        do {
+            firstNumber = +prompt(`Введите число`);
+        } while(!firstNumber);
+        do {
+            secondNumber = +prompt(`Введите второе число`);
+        } while(!secondNumber);
+        result = firstNumber * secondNumber;
+        operation = `Operation умножение, ${firstNumber} * ${secondNumber} = ${result}`;
+        myArray[myArray.length] = operation;
+        alert(`Operation умножение, ${firstNumber} * ${secondNumber} = ${result}`);
     } else if (choice == 4) {
-        alert(`Operation division finished with result ${result}`);
-        operation = `Operation division finished with result ${result}`;
-        myArray[myArray.length] = operation; 
+        do  {
+            firstNumber = +prompt(`Введите число`);
+        } while(!firstNumber);
+        do {
+            secondNumber = +prompt(`Введите второе число`);
+        } while(!secondNumber);
+        result = firstNumber / secondNumber;
+        operation = `Operation деление, ${firstNumber} / ${secondNumber} = ${result}`;
+        myArray[myArray.length] = operation;
+        alert(`Operation деление, ${firstNumber} / ${secondNumber} = ${result}`);
     } else if (choice == 5) {
-        alert(`Operation pow finished with result ${result}`);
-        operation = `Operation pow finished with result ${result}`;
-        myArray[myArray.length] = operation;  
+        do {
+            firstNumber = +prompt(`Введите основание степени`);
+        } while(!firstNumber);
+        do {
+            secondNumber = +prompt(`Введите показатель степени`);
+        } while(!secondNumber);
+        result = Math.pow(firstNumber, secondNumber);
+        operation = `Operation Pow, основание степени: ${firstNumber}, показатель степени: ${secondNumber} finished with result ${result}`;
+        myArray[myArray.length] = operation;
+        alert(`Operation Pow, основание степени: ${firstNumber}, показатель степени: ${secondNumber} finished with result ${result}`);
     } else if (choice == 6) {
-        alert(`Operation cos finished with result ${result}`);
-        operation = `Operation cos finished with result ${result}`;
-        myArray[myArray.length] = operation; 
+        do {
+            firstNumber = +prompt(`Введите число`);
+        } while(!firstNumber);
+        result = Math.cos(firstNumber);
+        operation = `Operation Cos, число: ${firstNumber} finished with result ${result}`;
+        myArray[myArray.length] = operation;
+        alert(`Operation Cos, число: ${firstNumber} finished with result ${result}`);
     } else if (choice == 7) {
-        alert(`Operation sin finished with result ${result}`);
-        operation = `Operation sin finished with result ${result}`;
-        myArray[myArray.length] = operation; 
+        do {
+            firstNumber = +prompt(`Введите число`);
+        } while((!firstNumber));
+        result = Math.sin(firstNumber);
+        operation = `Operation Sin, число: ${firstNumber} finished with result ${result}`;
+        myArray[myArray.length] = operation;
+        alert(`Operation Sin, число: ${firstNumber} finished with result ${result}`);
     } 
-    doItAgain = confirm(`Вы хотите совершить еще одну операцию?`);
+
+    if (choice == 8) {  
+        alert(`История операций: \n${myArray}`);
+    }
     
-} while(doItAgain === true)
+    doItAgain = confirm(`Вы хотите совершить еще одну операцию?`);
+
+} while(doItAgain === true);
