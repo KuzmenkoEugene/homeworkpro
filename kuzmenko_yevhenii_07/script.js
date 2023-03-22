7f8876cc6623988a5a3950466777a0a0ada3dff8
KuzmenkoEugene <evgenkuzmenko94@gmail.com>
2023-03-22 22:49:13 +0200

    changed my HW

--- kuzmenko_yevhenii_07/script.js
+++ kuzmenko_yevhenii_07/script.js
@@ -1,4 +1,4 @@
-let exampleArray = [10, 2, 3, 'hello', undefined, 65, 32, 1, 100]
+let exampleArray = [10, 2, 3, 'hello', undefined, 65, 32, 1, 100, 'three', 'world']
 
 let exampleArray2 = [1, 2, 3, 4, 5, 6, 15, 25]
 
@@ -7,11 +7,11 @@ const utils = {
     reverseArray: function () {
         let myArray = []
         for (i = 0; i < exampleArray.length; i++) {
-            myArray[i] = exampleArray[(exampleArray.length - 1) - i]
+            myArray[i] = exampleArray[(exampleArray.length - 1) - i] 
         }
         return myArray
     },
-    verifyNumbers: function myArrayNumbers () {
+    verifyNumbers: function () {
         let myArray = [];
         for (let i = 0; i < exampleArray.length; i++) {
             if (typeof exampleArray[i] === "number") {
@@ -28,18 +28,27 @@ const utils = {
             }
         }
         return min
-    },
+    }, 
     getAverage: function () {
         let averegeNumber = 0
         for (let i = 0; i < exampleArray2.length; i++) {
             averegeNumber += exampleArray2[i]
         }
         return averegeNumber / exampleArray2.length
+    },
+    getMaxstring: function () {
+        let myString = ''
+        for (let i = 0; i < exampleArray.length; i++) {
+            if (typeof exampleArray[i] === 'string' && exampleArray[i] > myString) {
+                myString = exampleArray[i]
+            }
+        }
+        return myString
     }
 }
 
 console.log(utils.reverseArray())
-console.log(utils.verifyNumbers())
+console.log(utils.verifyNumbers()) 
 console.log(utils.getMin())
-console.log(utils.getAverage(exampleArray2))
- 
+console.log(utils.getAverage())
+console.log(utils.getMaxstring())  
