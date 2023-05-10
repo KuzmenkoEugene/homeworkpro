02620168cbad4488736594fd4d386b243ea8e4bc
KuzmenkoEugene <evgenkuzmenko94@gmail.com>
2023-05-10 12:50:14 +0300

    add my HW13

---
+++ kuzmenko_yevhenii_13/script.js
@@ -0,0 +1,62 @@
+const emailInput = document.getElementById('email')
+const passwordInput = document.getElementById('password')
+const errorMessageEmail = document.getElementById('errorEmail')
+const errorMessagePassword = document.getElementById('errorPassword')
+const button = document.getElementById('btn')
+
+function disabledButton() {
+    if (emailInput.value === '') {
+        button.disabled = true;
+    } else {
+        button.disabled = false;
+    }
+}
+
+emailInput.addEventListener('input', disabledButton);
+
+disabledButton()
+
+//
+
+document.getElementById('email').addEventListener('focus', () => {
+    
+})
+
+document.getElementById('email').addEventListener('blur', () => {
+    checkEmail()
+})
+
+document.getElementById('password').addEventListener('focus', () => {
+    
+})
+
+document.getElementById('password').addEventListener('blur', () => {
+    checkPassword()
+})
+
+function checkEmail() {
+    let myValue = emailInput.value
+    
+
+    if(myValue.includes("@")) {
+        errorMessageEmail.textContent = ''
+        emailInput.style.color = 'black'
+    } else {
+        errorMessageEmail.textContent = 'Wrong email!'
+        emailInput.style.color = 'red'
+    }
+    
+}
+
+function checkPassword() {
+    if(passwordInput.value.length < 6) {
+        errorMessagePassword.textContent = 'Password has less than 6 characters'
+        passwordInput.style.color = 'red'
+    } else {
+        errorMessagePassword.textContent = ''
+        passwordInput.style.color = 'black'
+    }
+}
+
+
+
