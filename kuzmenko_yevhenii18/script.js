ad2f35ede53fb637f92114b5f84f762948a769fe
Eugene <116023701+KuzmenkoEugene@users.noreply.github.com>
2023-06-18 15:52:20 +0300

    Add files via upload

--- kuzmenko_yevhenii18/script.js
+++ kuzmenko_yevhenii18/script.js
@@ -1,3 +1,4 @@
+
 class MyPersonsList {
     constructor(yourList) {
         this.yourList = yourList;
@@ -5,24 +6,30 @@ class MyPersonsList {
         this.buttonClickPrevious = document.getElementById('buttonPrevious');
         this.buttonClickNext = document.getElementById('buttonNext');
         this.removeCards = document.getElementById('my_list__person')
-        this.xhr = new XMLHttpRequest();
         this.globalIndex = 1;
         this.quantityIndecator = this.contentList.children.length;
+        this.myList
+        this.creatCards()
+        this.click()
+    }
+
+    fetch() {
+        this.xhr = new XMLHttpRequest();
+        this.xhr.open('GET', `https://reqres.in/api/users?page=${this.globalIndex}`, false);
+        this.xhr.send()
+        this.myList = JSON.parse(this.xhr.response)
     }
 
     creatCards() {
-        this.xhr.open('GET', `https://reqres.in/api/users?page=${this.globalIndex}`, false);
-            this.xhr.send()
+            this.fetch()
 
-            let myList = JSON.parse(this.xhr.response)
-
-            for (let index = 0; index < myList.data.length; index++) {
+            for (let index = 0; index < this.myList.data.length; index++) {
 
                 let item = document.createElement('div')
                 item.classList.add('my_list__person')
                 this.contentList.appendChild(item)
 
-                let avatarPerson = document.createElement('p')
+                let avatarPerson = document.createElement('img')
                 avatarPerson.classList.add('person__avatar')
                 item.appendChild(avatarPerson)
 
@@ -42,44 +49,150 @@ class MyPersonsList {
                 idPerson.classList.add('person__id')
                 item.appendChild(idPerson)
 
-                avatarPerson.innerText = JSON.stringify(myList.data[index].avatar);
-                emailPerson.innerText = JSON.stringify(myList.data[index].email);
-                firstNamePerson.innerText = JSON.stringify(myList.data[index].first_name);
-                lastnNamePerson.innerText  = JSON.stringify(myList.data[index].last_name);
-                idPerson.innerText = JSON.stringify(myList.data[index].id);
+                let avatarPersonUrl = JSON.stringify(this.myList.data[index].avatar);
+                avatarPerson.src = avatarPersonUrl.replace(/"/g, '')
+
+                let emailPersonText = JSON.stringify(this.myList.data[index].email);
+                emailPersonText = "Email: " + emailPersonText;
+                emailPerson.innerText = emailPersonText.replace(/"/g, '')
+
+                let firstNamePersonText = JSON.stringify(this.myList.data[index].first_name);
+                firstNamePersonText = "First name: " + firstNamePersonText;
+                firstNamePerson.innerText = firstNamePersonText.replace(/"/g, '')
+
+                let lastnNamePersonText = JSON.stringify(this.myList.data[index].last_name);
+                lastnNamePersonText = "Last name: " + lastnNamePersonText;
+                lastnNamePerson.innerText = lastnNamePersonText.replace(/"/g, '')
+
+
+                let idPersonText = JSON.stringify(this.myList.data[index].id);
+                idPersonText = "ID: " + idPersonText;
+                idPerson.innerText = idPersonText
+                
             }
     }
 
-    click = () => {
+    remove = () => {
         let quantityIndecator = this.contentList.children.length;
 
-        this.buttonClickPrevious.addEventListener('click', () => {
-            for (let index = 0; index < quantityIndecator; index++) {
-                this.contentList.children[0].remove()                
-            }
+        for (let index = 0; index < quantityIndecator; index++) {
+            this.contentList.children[0].remove()                
+        } 
+    }
+
+    click = () => {
+         
+        this.clickBack = () => {
+            this.remove()
 
             this.globalIndex--;
 
             this.creatCards()
-        })
 
-        this.buttonClickNext.addEventListener('click', () => {
-            for (let index = 0; index < quantityIndecator; index++) {
-                this.contentList.children[0].remove()                
+            if(this.globalIndex === 1) {
+                this.buttonClickPrevious.disabled = true;
+                this.buttonClickNext.disabled = false;
             }
+        }
+
+        this.clickNext = () => {
+            this.remove()
 
             this.globalIndex++;
 
             this.creatCards()
-        })
+
+            if(this.globalIndex === 2) {
+                this.buttonClickPrevious.disabled = false;
+                this.buttonClickNext.disabled = true;
+            }
+        }
+
+        this.buttonClickPrevious.addEventListener('click', this.clickBack)
+        this.buttonClickNext.addEventListener('click', this.clickNext)
     }
 }
 
 const listEl = document.getElementById('yourList')
 const myPersonsList = new MyPersonsList(listEl)
 
-myPersonsList.creatCards()
-myPersonsList.click()
+class NewPerson {
+    constructor(myNewCards) {
+        this.myNewCards = myNewCards;
+        this.contentList = document.getElementsByClassName('my_newlist')[0];
+        this.firstNameInput = document.getElementById('addFirstNamePerson')
+        this.lastNameInput = document.getElementById('addLastNamePerson')
+        this.emailInput = document.getElementById('addYourEmail')
+        this.jobInput = document.getElementById('addYourJob')
+        this.myButton = document.getElementById('addNewPersonButton')
+        this.dataPerson
+        
+    }
 
+    fetch() {
+        this.xhr = new XMLHttpRequest();
+        this.xhr.open('POST', `https://reqres.in/api/users`, false);
+        this.xhr.setRequestHeader('Content-type', 'application/json')
+    }
 
+    creatNewUser() {
+        
+        this.fetch()
 
+        this.myButton.addEventListener('click', (e) => {
+            e.preventDefault()
+
+            let newCard = {
+                name: this.firstNameInput.value,
+                lastName: this.lastNameInput.value,
+                email: this.emailInput.value,
+                job: this.jobInput.value
+            }
+    
+            this.xhr.send(JSON.stringify(newCard))
+    
+            this.dataPerson = JSON.parse(this.xhr.response)
+
+            let item = document.createElement('div')
+            item.classList.add('my_list__person')
+            this.contentList.appendChild(item)
+
+            let avatarPerson = document.createElement('img')
+            avatarPerson.classList.add('person__avatar')
+            avatarPerson.src = 'https://www.gateshead.church/dowo/wp-content/uploads/2019/09/person-icon.png'
+            item.appendChild(avatarPerson)
+
+            let firstNamePerson = document.createElement('p')
+            firstNamePerson.classList.add('person__first_name')
+            firstNamePerson.innerText = 'First name: ' + this.dataPerson.name,
+            item.appendChild(firstNamePerson)
+
+            let lastnNamePerson = document.createElement('p')
+            lastnNamePerson.classList.add('person__lastname')
+            lastnNamePerson.innerText = 'Last name: ' + this.dataPerson.lastName,
+            item.appendChild(lastnNamePerson)
+
+            let emailPerson = document.createElement('p')
+            emailPerson.classList.add('person__email')
+            emailPerson.innerText = 'Email: ' + this.dataPerson.email,
+            item.appendChild(emailPerson)
+
+            let jobPerson = document.createElement('p')
+            jobPerson.classList.add('person__email')
+            jobPerson.innerText = 'Job: ' + this.dataPerson.job,
+            item.appendChild(jobPerson)
+
+            this.firstNameInput.value = '',
+            this.lastNameInput.value = '',
+            this.emailInput.value = '',
+            this.jobInput.value = ''
+        })
+
+        
+    }
+}
+
+const myNewCardsList = document.getElementById('myNewCards')
+const list = new NewPerson (myNewCardsList)
+
+list.creatNewUser()
