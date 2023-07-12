ed923e68e36628b64c31338efa310c4e31d80e7f
Eugene <116023701+KuzmenkoEugene@users.noreply.github.com>
2023-07-12 13:44:02 +0300

    Add files via upload

--- kuzmenko__yevhenii22/gulpfile.js
+++ kuzmenko__yevhenii22/gulpfile.js
@@ -1,12 +1,12 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const uglify = require('gulp-uglify');
-
+const clean = require('gulp-clean');
 
 function buildStyles() {
     return gulp.src('style/main.scss')
       .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-      .pipe(gulp.dest('./dist'));
+      .pipe(gulp.dest('./dist/css'));
 };
 
 function watchMinifyJS() {
@@ -32,9 +32,15 @@ function watchHTML() {
     return gulp.watch('./index.html', modelIndexHTML)
 }
 
+function removeDist() {
+    return gulp.src('./dist')
+        .pipe(clean())
+}
+
 exports.buildStyles = buildStyles;
 exports.watchStyles = watchStyles;
 exports.minifyJS = minifyJS;
-exports.watchMinifyJS = watchMinifyJS
-exports.watchHTML = watchHTML
+exports.watchMinifyJS = watchMinifyJS;
+exports.watchHTML = watchHTML;
+exports.removeDist = removeDist;
 exports.build = gulp.series(modelIndexHTML, gulp.parallel(buildStyles, minifyJS))
